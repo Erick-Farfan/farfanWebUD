6b540b83748b0fb74d3a1916b0bc132b65f52af6
Erick-Farfan <emfarfand@udistrital.edu.co>
2026-05-11 22:29:52 -0500

    dr_cdt_CDTDAO

---
+++ src/main/java/cdt/modelo/CDTDAO.java
@@ -0,0 +1,8 @@
+package cdt.modelo;
+// Autor: Erick Mauricio Farfán Díaz
+import java.util.ArrayList;
+
+public class CDTDAO {
+    // Lista estática que persiste durante toda la vida de la aplicación
+    public static ArrayList<CDT> lista_C = new ArrayList<>();
+}
